b30afe88afde4af082230b76ee6518bff97782e8
BilalWazir033 <bilalwaziri2006@gmail.com>
2026-05-19 10:18:54 +0500

    API creation by Node.js and Express.js

---
+++ Javascript/BasicsOfJavascript.js
@@ -0,0 +1,11 @@
+console.log('Welcome to waziristan razmak');
+let a='My World';
+console.log(a)
+let b=5;
+let c=9; // global 
+console.log('sum',b+c);
+let name='Ilham';
+let age=23;
+console.log('Name:',name,'Age:',age);
+var d=34; // local 
+console.log('sub',d-c);
