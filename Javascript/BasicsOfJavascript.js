5e04a0ca4d3c597da7b8bb367a3e0f6432f1d3e4
BilalWazir033 <bilalwaziri2006@gmail.com>
2026-06-12 23:22:24 +0500

    javascript basics

--- Javascript/BasicsOfJavascript.js
+++ Javascript/BasicsOfJavascript.js
@@ -11,6 +11,16 @@ var d=34; // local
 console.log('sub',d-c);
 let person={name:"Ali", age:23}
 console.log(person)
-let age=20;
+let Age=20;
 let isadult=(Age>=18)? 'Yes':'No';
-consule
+console.log(isadult);
+let num=18;
+if(num<18){
+    console.log("You can't cast vote");
+}
+else if(num>=18 && num<60){
+    console.log("You are eligible to cast vote");
+}
+else{
+    console.log("you are too old. Rest please");
+}
