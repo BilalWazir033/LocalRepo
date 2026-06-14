07eb69275c82646a25f239fbc35ac87063c98133
BilalWazir033 <bilalwaziri2006@gmail.com>
2026-06-14 12:09:02 +0500

    Express in mern

--- Javascript/mern_node.js
+++ Javascript/mern_node.js
@@ -3,3 +3,46 @@ const fs = require("fs").promises;
 fs.readFile("data.txt", "utf8")
   .then(data => console.log(data))
   .catch(err => console.log(err));
+
+  const p1 = new Promise(resolve =>
+  setTimeout(() => resolve("First"), 1000)
+);
+
+const p2 = new Promise(resolve =>
+  setTimeout(() => resolve("Second"), 2000)
+);
+
+Promise.race([p1, p2])
+  .then(result => console.log(result));
+
+
+
+  const fss = require("fs").promises;
+
+async function readData() {
+    try {
+        const data = await fss.readFile("data.txt", "utf8");
+        console.log(data);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+readData();
+
+//  call back 
+fs.writeFile("test.txt", "Hello MY WORLD", (err) => {
+    if (err) throw err;
+    console.log("File Written");
+});
+
+
+// ASYC/WAIT
+const fsss = require("fs").promises;
+
+async function writeData() {
+    await fs.writeFile("text.txt", "Hello");
+    console.log("Done");
+}
+
+writeData();    
