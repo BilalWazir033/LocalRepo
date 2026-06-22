6f64dc475e2cb8478033428485bbe51992f0285d
BilalWazir033 <bilalwaziri2006@gmail.com>
2026-06-22 23:54:23 +0500

    .

--- Javascript/WordsCountProject/WordsCountProject.cpp
+++ Javascript/WordsCountProject/WordsCountProject.cpp
@@ -28,7 +28,7 @@ int main() {
     map<string, int> wordFrequency;
 
     set<string> stopWords = {
-        "the", "and", "is", "of", "to", "in", "a", "an", "for", "on", "with","were","from"
+        "the", "and", "is", "of", "to", "in", "a", "an", "for", "on", "with","were"
     };
 
     cout << "Enter input file name: ";
